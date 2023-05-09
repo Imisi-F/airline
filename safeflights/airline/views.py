da023118b9dc4fc200caafcd71e5905b3676ab25
Imisi-F <sc20if@leeds.ac.uk>
2023-05-09 19:57:38 +0100

    trying to fix module not found error

--- safeflights/airline/views.py
+++ safeflights/airline/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
