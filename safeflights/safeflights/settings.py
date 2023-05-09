550ebbe19f1e431e3c3d92cbe8bb1447aa8e3a9c
Imisi-F <sc20if@leeds.ac.uk>
2023-05-09 05:22:33 +0100

    populated db

--- safeflights/safeflights/settings.py
+++ safeflights/safeflights/settings.py
@@ -37,8 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'airline.apps.AirlineConfig'
+    'airline.apps.AirlineConfig',
+    'rest_framework'
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
