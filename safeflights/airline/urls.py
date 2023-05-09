dd2938ae4bbf19ca507073beb27f15bf08084629
Imisi-F <sc20if@leeds.ac.uk>
2023-05-09 18:42:15 +0100

    Completed and tested view functions

--- safeflights/airline/urls.py
+++ safeflights/airline/urls.py
@@ -3,8 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.allFlights, name='all_flights'),
+    
     path('flights/departure/<str:departure>/destination/<str:destination>/departure_date/<str:departure_date>/seat_class/<str:seat_class>/ticket_adult/<int:ticket_adult>/ticket_child/<int:ticket_child>/', views.getFlightsInfo, name='flights'),
+    
     path('flights/<int:flight_id>/seats/<str:seat_codes>/', views.selectFlightSeat, name='select_flight_seat'),
-    path('flights/<int:flight_id>/bookings/', views.confirmFlightSeat, name='confirm_booking')
+    
+    path('flights/<int:flight_id>/booking_id/<int:booking_id>/transaction_id/<int:transaction_id>/seats/<str:seat_codes>/email/<str:email>/number_tickets/<int:number_tickets>/total_price/<str:total_price>/cancelled/<str:cancelled>/', views.confirmFlightSeat, name='confirm_booking')
 ]
-# flights/departure/JFK/destination/LAX/departure_date/2023-05-10/seat_class/first/ticket_adult/2/ticket_child/1/
+# To test getFlightsInfo:
+# http://127.0.0.1:8000/airline/flights/departure/JFK/destination/LAX/departure_date/2023-05-10/seat_class/first/ticket_adult/2/ticket_child/1/
+
+# To test selectFlightSeat:
+# http://127.0.0.1:8000/airline/flights/11/seats/sy,sw,sx
+
+# To test confirmFlightSeat:
+# http://127.0.0.1:8000/airline/flights/11/booking_id/200/transaction_id/200/seats/sw,sx/email/example@example.com/number_tickets/2/total_price/1000/cancelled/False/
--- safeflights/airline/views.py
+++ safeflights/airline/views.py
@@ -7,47 +7,40 @@ from rest_framework.response import Response
 from .models import FlightInfo, PassengerSeatInfo, Bookings,Airports
 from datetime import datetime, timedelta
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 @api_view(['GET'])
 def allFlights(request):
   all_flights = [flight.flight_number for flight in FlightInfo.objects.all()]
-  # return JsonResponse({'all_flights': all_flights}, status=status.HTTP_202_ACCEPTED))
-  return Response(all_flights, status=status.HTTP_202_ACCEPTED)
+  return Response(all_flights, status=status.HTTP_200_OK)
 
-
-# use @csrf_exempt if you wanna use post requests
-  # Assuming info comes as a string in format (NO SPACES):
-  # [departure/<str:departure>/destination/<str:destination>/departure_date/<str:departure_date in dd-mm-yyyy>/seat_class/<str:seat_class>/ticket_adult/<int:ticket_adult>/ticket_child/<int:ticket_child>/]
-  # retrieve the user's search parameters from the query string
+# retrieve the user's search parameters from the query string
 @api_view(['GET'])
 def getFlightsInfo (request, departure, destination, departure_date, seat_class, ticket_adult, ticket_child):
   if request.method == 'GET':
-    print("{}, {}, {}, {}, {}".format, destination, departure,seat_class,ticket_adult, ticket_child)
+    # Setting user params for flight
     departure_airport = str(departure).upper()
     destination_airport = str(destination).upper()
     departure_date = departure_date
     class_multiplier = 1.0
     if 'first' in str(seat_class).lower():
-      class_multiplier = 2.0
+      class_multiplier = 5.0
     if 'business' in str(seat_class).lower():
-      class_multiplier = 1.5
-    print("{}".format, class_multiplier)
+      class_multiplier = 2.5
     no_of_tickets = ticket_adult + ticket_child
     # Filter based on user input  
     flight_info_list = FlightInfo.objects.all().filter(departure_airport=departure_airport, destination_airport=destination_airport, departure_date__contains=departure_date)
     # Make flights info a JSON obj, include calculated flight duration and flight price
     flights_info_json = []
-    
-    print("okay")
     for flight in flight_info_list:
-      # len(flight.seat_list.split(','))
       # if there's enough available seats on the flight
       if flight.seat_list.count('0') >= no_of_tickets:
-        # calculate the total duration of the flight
-        hour = abs(flight.arrival_date.time().hour - flight.departure_date.time().hour) * 60
-        minute = abs(flight.arrival_date.time().minute  - flight.departure_date.time().minute)
-        second = abs(flight.arrival_date.time().second - flight.departure_date.time().second) / 60
-        flight_time = hour + minute + second
+        # calculate the total duration of the flight in minute
+        d_time = flight.departure_date
+        a_time  = flight.arrival_date
+        duration = a_time - d_time
+        s_duration = duration.total_seconds()
+        flight_time = divmod(s_duration, 60)[0]
         # Child price is 50% of adults
         flight_price =( (float(flight.flight_price) * 
         float(ticket_adult)) + (float(flight.flight_price) * 0.5 * float(ticket_child))) * class_multiplier
@@ -57,90 +50,90 @@ def getFlightsInfo (request, departure, destination, departure_date, seat_class,
   else:
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
-# @csrf_exempt
-@api_view(['POST'])
+@csrf_exempt
+@api_view(['GET','POST'])
 def selectFlightSeat (request, flight_id, seat_codes):
-  if request.method == 'POST':
+  if request.method == 'GET' or request.method == 'POST':
     try:
       seats = FlightInfo.objects.all().get(id=flight_id)
-    except FlightInfo.DoesNotExist:
-        return HttpResponse('404_NOT_FOUND')
-    #shows there are some seats
+    except FlightInfo.objects.all().get(id=flight_id).DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    # checks if there are some seats
     if str(seats.seat_list).count('0') < 1:
-      return HttpResponse('No seats available')
+      return Response('No seats available')
     # seat format setcode:takenno, setcode2:takenno2,
     seats_status = {}
     taken_flag = 0
     list_seats = str(seats.seat_list).split(',')
-    # seeing if the seats are taken or not
-    for word in list_seats:
-      for code in seat_codes:
-        if code.lower() in word.lower() and "0" in word:
-          seats_status[code] = 'Taken'
-        else:
-          seats_status[code] = 'Not Taken'
+    # seeing if all the seats are taken or not
+    for code in seat_codes.split(','):
+      for word in list_seats:
+        if code == word.split(':')[0]:
+          if word.split(':')[1] == '1':
+            seats_status[code] = 'Taken'
+          elif word.split(':')[1] == '0':
+            seats_status[code] = 'Not Taken'
     for x in seats_status.values():
       if x == 'Taken':
         taken_flag += 1
     if taken_flag > 0:
-      # if seats are taken:
-      return seats_status.items(),HttpResponse("\n{} seat(s) are taken\nStatus Code: 500".format(taken_flag))
+      # if any seats are taken:
+      return Response(seats_status.items(),status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     else:
-      # reserving specified empty seats depending on user input
-      reserved = str(seats.seat_list).split(',')
-      for i in range(len(reserved)):
-        for code in seat_codes:
-          if code in reserved[i]:
-            reserved[i] = "{}:{}".format(code,'1')
-      new_seats = ','.join(reserved)
-      seats.seat_list = new_seats
-      seats.save()
-      return JsonResponse({'seats': seats_status.values()}), HttpResponse("All seat(s) have been reserved.\nStatus Code: 200")
+      return Response({'seats': seats_status.items()}, status=status.HTTP_200_OK)
 
-# @csrf_exempt
-@api_view(['POST'])
-def confirmFlightSeat (request, flight_id, booking_info):
-  if request.method == 'POST':
+@csrf_exempt
+@api_view(['GET','POST'])
+def confirmFlightSeat (request, flight_id, booking_id,transaction_id, seat_codes, email, number_tickets, total_price, cancelled):
+  if request.method == 'POST' or request.method == 'GET':
     try:
       flight_info = FlightInfo.objects.all().get(id=flight_id)
-    except FlightInfo.DoesNotExist:
-      return HttpResponse('404_NOT_FOUND')
-    # creating a new booking
-    booking = Bookings.objects.create(
-    id=booking_info['booking_id'], 
-    transaction_id=booking_info['transaction_id'], 
-    seat_codes=booking_info['seat_codes'],
-    email=booking_info['email'],
-    number_tickets=booking_info['number_tickets'],
-    total_price=booking_info['total_price'],
-    cancelled=booking_info['cancelled'])
-    return HttpResponse('Added new booking. Status: 200'), flight_info.seat_list
+    except FlightInfo.objects.all().get(id=flight_id).DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
+    # if booking has not been cancelled
+    if cancelled.lower() == 'false':
+      # creating a new booking
+      booking = Bookings.objects.create(
+      id=booking_id, 
+      transaction_id=float(transaction_id), 
+      seat_codes=seat_codes,
+      email=email,
+      number_tickets=number_tickets,
+      total_price=total_price,
+      cancelled=cancelled)
+      booking.save()
+      # updating the seat_list in the flight's info
+      seats_update = flight_info.seat_list.split(',')
+      i = 0
+      for seat in seats_update:
+        for code in seat_codes.split(','):
+          if code == seat.split(':')[0]:
+            temp = seat.split(':')[1].replace('0', '1')
+            update_seat = "{}:{}".format(code,temp)
+            seats_update[i] = update_seat
+        i += 1
+      flight_info.seat_list = ','.join(seats_update)
+      flight_info.save()
+      return Response(flight_info.seat_list, status=status.HTTP_200_OK)
+    # if booking has not been cancelled
+    elif cancelled.lower() == 'true':
+      # setting cancelled flag to true
+      booking = Bookings.objects.all().get(id=booking_id)
+      booking.cancelled = cancelled
+      booking.save()
+      # updating the seat_list in the flight's info
+      seats_update = flight_info.seat_list.split(',')
+      i = 0
+      for seat in seats_update:
+        for code in seat_codes.split(','):
+          if code == seat.split(':')[0]:
+            temp = seat.split(':')[1].replace('1', '0')
+            update_seat = "{}:{}".format(code,temp)
+            seats_update[i] = update_seat
+        i += 1
+      flight_info.seat_list = ','.join(seats_update)
+      flight_info.save()
+      return Response(flight_info.seat_list, status=status.HTTP_200_OK)
 
-
-
-'''flight_info_isolated = [] 
-    flight_info = list(request.body).split(',')
-    ticket_adult = int(flight_info[4])
-    ticket_child = int(flight_info[5])
-    no_of_tickets = ticket_adult + ticket_child
-    for info in flight_info:
-      info_isolated = info.split('=')
-      flight_info_isolated.append(str(info_isolated[1]))
-    # Filter based on user input  
-    flight_info_list = FlightInfo.objects.all().filter(departure_airport=flight_info_isolated[0], destination_airport=flight_info_isolated[1], departure_date=flight_info_isolated[2], seat_class__contains=flight_info_isolated[3])
-    # Make flights info a JSON obj, include calculated flight duration and flight price
-    flights_info_json = []
-    if len(flight_info_list) > no_of_tickets:
-      for flight in flight_info_list:
-        hour = abs(flight(name='arrival_date').time().hour - flight(name='departure_date').time().hour) *60
-        minute = abs(flight(name='arrival_date').time().minute - flight(name='departure_date').time().minute)
-        second = abs(flight(name='arrival_date').time().second - flight(name='departure_date').time().second) /60
-        flight_time_mins = hour + minute + second
-        # Child price is 50% of adults
-        flight_price = (flight(name='flight_price') * 
-        ticket_adult) + (flight(name='flight_price') * 0.5 * ticket_child)
-        # Adding to the json-style list 
-        flights_info_json.append("id:{},flight_number:{},seats_available:{},departure_airport:{},destination_airport:{},departure_date:{},arrival_date:{},flight_time_mins:{},flight_price:{}".format(flight(name='id'), flight(name='flight_number'), flight(name='seat_list'), flight(name='departure_airport'), flight(name='destination_airport'), flight(name='departure_date'), flight(name='arrival_date'), flight_time_mins, flight_price))
-      return flights_info_json, HttpResponse.status_code
     else:
-      return HttpResponse('No flights available')'''
+      return Response(flight_info.seat_list, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
